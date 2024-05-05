a6f3c0d5ac29176c03f73352bd735b38a1dea0bd
Nikolay Fayustov <n.a.fayustov@gmail.com>
2024-05-05 14:46:32 +0300

    deploy test

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/nutrition-app/',
   optimizeDeps: {
     exclude: ['js-big-decimal']
   },
