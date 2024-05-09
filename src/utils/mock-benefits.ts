8e2588ce9f6a8386d672d56174762fbad789dcf7
Nikolay Fayustov <n.a.fayustov@gmail.com>
2024-05-09 19:11:42 +0300

    img fix

--- src/utils/mock-benefits.ts
+++ src/utils/mock-benefits.ts
@@ -1,6 +1,6 @@
 export const dataSet = [
     {
-        img: '../nutrition-app/src/images/icons-benefits/calculator.svg',
+        img: '../nutrition-app/public/assets/images/icons-benefits/calculator.svg',
         text: 'Научишься считать калории'
     },
     {
@@ -8,43 +8,43 @@ export const dataSet = [
         text: 'Найдешь свое соотношение БЖУ'
     },
     {
-        img: '../../src/images/icons-benefits/crosshair.png',
+        img: '../nutrition-app/src/images/icons-benefits/crosshair.png',
         text: 'Научишься составлять рацион под цель'
     },
     {
-        img: '../../src/images/icons-benefits/down-arrow.png',
+        img: '../nutrition-app/src/images/icons-benefits/down-arrow.png',
         text: 'Обновишь гардероб на пару размеров'
     },
     {
-        img: '../../src/images/icons-benefits/rocket.svg',
+        img: '../nutrition-app/src/images/icons-benefits/rocket.svg',
         text: 'Сделаешь лучшую физическую форму'
     },
     {
-        img: '../../src/images/icons-benefits/box.png',
+        img: '../nutrition-app/src/images/icons-benefits/box.png',
         text: 'Настроишь вариативность рациона'
     },
     {
-        img: '../../src/images/icons-benefits/stop.png',
+        img: '../nutrition-app/src/images/icons-benefits/stop.png',
         text: 'Разрушишь мифы вокруг питания'
     },
     {
-        img: '../../src/images/icons-benefits/calendar.png',
+        img: '../nutrition-app/src/images/icons-benefits/calendar.png',
         text: 'Поймешь, как сохранять и быть в форме круглый год'
     },
     {
-        img: '../../src/images/icons-benefits/mineral.png',
+        img: '../nutrition-app/src/images/icons-benefits/mineral.png',
         text: 'Закроешь нормы витаминов и минералов'
     },
     {
-        img: '../../src/images/icons-benefits/fire.png',
+        img: '../nutrition-app/src/images/icons-benefits/fire.png',
         text: 'Поймешь, когда нужно использовать спортпит или БАД'
     },
     {
-        img: '../../src/images/icons-benefits/circle.png',
+        img: '../nutrition-app/src/images/icons-benefits/circle.png',
         text: 'Взглянешь на образ жизни по-новому'
     },
     {
-        img: '../../src/images/icons-benefits/check.png',
+        img: '../nutrition-app/src/images/icons-benefits/check.png',
         text: 'Поймешь, что все намного проще чем ты думал (-а)'
     },
 ]
