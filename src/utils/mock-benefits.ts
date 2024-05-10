a1df49a0fae876a3cbf60c3b0ecab55dc0d909f8
Nikolay Fayustov <n.a.fayustov@gmail.com>
2024-05-10 08:40:20 +0300

    img 1

--- src/utils/mock-benefits.ts
+++ src/utils/mock-benefits.ts
@@ -4,7 +4,7 @@ export const dataSet = [
         text: 'Научишься считать калории'
     },
     {
-        img: 'nutrition-app/images/icons-benefits/p-cart.png',
+        img: 'nutrition-app/dist/images/icons-benefits/p-cart.png',
         text: 'Найдешь свое соотношение БЖУ'
     },
     {
@@ -12,39 +12,39 @@ export const dataSet = [
         text: 'Научишься составлять рацион под цель'
     },
     {
-        img: 'nutrition-app/src/images/icons-benefits/down-arrow.png',
+        img: 'nutrition-app/dist/images/icons-benefits/down-arrow.png',
         text: 'Обновишь гардероб на пару размеров'
     },
     {
-        img: '../nutrition-app/src/images/icons-benefits/rocket.svg',
+        img: 'nutrition-app/dist/images/icons-benefits/rocket.svg',
         text: 'Сделаешь лучшую физическую форму'
     },
     {
-        img: '../nutrition-app/src/images/icons-benefits/box.png',
+        img: 'nutrition-app/dist/images/icons-benefits/box.png',
         text: 'Настроишь вариативность рациона'
     },
     {
-        img: '../nutrition-app/src/images/icons-benefits/stop.png',
+        img: 'nutrition-app/dist/images/icons-benefits/stop.png',
         text: 'Разрушишь мифы вокруг питания'
     },
     {
-        img: '../nutrition-app/src/images/icons-benefits/calendar.png',
+        img: 'nutrition-app/dist/images/icons-benefits/calendar.png',
         text: 'Поймешь, как сохранять и быть в форме круглый год'
     },
     {
-        img: '../nutrition-app/src/images/icons-benefits/mineral.png',
+        img: 'nutrition-app/dist/images/icons-benefits/mineral.png',
         text: 'Закроешь нормы витаминов и минералов'
     },
     {
-        img: '../nutrition-app/src/images/icons-benefits/fire.png',
+        img: 'nutrition-app/dist/images/icons-benefits/fire.png',
         text: 'Поймешь, когда нужно использовать спортпит или БАД'
     },
     {
-        img: '../nutrition-app/src/images/icons-benefits/circle.png',
+        img: 'nutrition-app/dist/images/icons-benefits/circle.png',
         text: 'Взглянешь на образ жизни по-новому'
     },
     {
-        img: '../nutrition-app/src/images/icons-benefits/check.png',
+        img: 'nutrition-app/dist/images/icons-benefits/check.png',
         text: 'Поймешь, что все намного проще чем ты думал (-а)'
     },
 ]
