6f2afef9d2883f4e8412dd0efbe0fca5a37bdbae
Nikolay Fayustov <n.a.fayustov@gmail.com>
2024-06-15 09:29:29 +0300

    калькулятор % жира

--- src/pages/сalculators/сalculators.tsx
+++ src/pages/сalculators/сalculators.tsx
@@ -11,11 +11,41 @@ export const statusList = [
 ]
 export const Сalculators = () => {
 
-    const [result, setResult] = useState(null);
+    const [resultNavy, setResultNavy] = useState(null);
+    const [resultBMI, setResultBMI] = useState(null);
+    const [resultFatMass, setresultFatMass] = useState(null);
+    const [fatCategory, setfatCategory] = useState(null);
+    const [error, setError] = useState(null);
 
     const { values, handleChange } = useForm({
         gender: 'Мужской',
     })
+
+    function getFatCategory(gender: any, value: any) {
+        const classFatListMale = [
+            { value: 5, nameValue: 'Необходимый жир' },
+            { value: 13, nameValue: 'Атлетическое телосложение' },
+            { value: 17, nameValue: 'Спортивное телосложение' },
+            { value: 24, nameValue: 'Обычное телосложение' },
+            { value: 25, nameValue: 'Ожирение' },
+        ];
+
+        const classFatListFemale = [
+            { value: 10, nameValue: 'Необходимый жир' },
+            { value: 14, nameValue: 'Атлетическое телосложение' },
+            { value: 21, nameValue: 'Спортивное телосложение' },
+            { value: 25, nameValue: 'Обычное телосложение' },
+            { value: 32, nameValue: 'Ожирение' },
+        ];
+
+        const classFatList = gender === 'Мужской' ? classFatListMale : classFatListFemale;
+
+        for (let i = 0; i < classFatList.length; i++) {
+            if (value <= classFatList[i].value) {
+                return classFatList[i].nameValue;
+            }
+        } return classFatList[classFatList.length - 1].nameValue;
+    }
 
 
     const calculateFatPercentage = () => {
@@ -27,18 +57,34 @@ export const Сalculators = () => {
             values.age !== undefined &&
             values.neck !== undefined &&
             values.height !== undefined &&
+            values.weight !== undefined &&
             (values.gender === 'Мужской' || (values.gender === 'Женский' && values.hips !== undefined))
         ) {
-            let fatPercentage: any;
+            let fatPercentageNavy: any;
+            let fatPercentageBMI: any;
             if (values.gender === 'Мужской') {
-                fatPercentage = 495 / (1.0324 - 0.19077 * log10(values.waist - values.neck) + 0.15456 * log10(values.height)) - 450;
+                fatPercentageNavy = 495 / (1.0324 - 0.19077 * log10(values.waist - values.neck) + 0.15456 * log10(values.height)) - 450;
+                fatPercentageBMI = (1.2 * (values.weight / (values.height / 100 * values.height / 100))) + 0.23 * values.age - 16.2;
             } else {
-                fatPercentage = 495 / (1.29579 - 0.35004 * log10(Number(values.waist) + Number(values.hips!) - Number(values.neck)) + 0.22100 * log10(Number(values.height))) - 450;
+                fatPercentageNavy = 495 / (1.29579 - 0.35004 * log10(Number(values.waist) + Number(values.hips!) - Number(values.neck)) + 0.22100 * log10(Number(values.height))) - 450;
+                fatPercentageBMI = (1.2 * (values.weight / (values.height / 100 * values.height / 100))) + 0.23 * values.age - 5.4;
             }
-            setResult(fatPercentage.toFixed(2));
+
+            if (fatPercentageNavy > 0 && fatPercentageBMI > 0) {
+                const fatCat: any = getFatCategory(values.gender, fatPercentageNavy)
+                setfatCategory(fatCat)
+                setResultBMI(fatPercentageBMI.toFixed(2))
+                setResultNavy(fatPercentageNavy.toFixed(2));
+                const resultFatMass: any = values.weight * fatPercentageNavy / 100;
+                setresultFatMass(resultFatMass.toFixed(2))
+            }
+
         } else {
-            console.error('Некоторые значения отсутствуют или не определены.');
-            setResult(null);
+            const error: any = 'Некоторые значения отсутствуют или введены некорректные данные!'
+            setError(error)
+            setResultNavy(null);
+            setResultBMI(null);
+            setresultFatMass(null);
         }
     };
 
@@ -153,12 +199,42 @@ export const Сalculators = () => {
                             </div>
 
                         </div>
-                        <div className={styles.result}>
+                        <div className={styles.results}>
                             <h3 className={styles.h3}>Ваш результат</h3>
-                            <div className={styles.fat}>{result}</div>
-                            <div className={styles.fatmass}></div>
-                            <div className={styles.mass}></div>
-                            <div className={styles.norms}></div>
+                            {error ? (
+                                <div className={styles.error}>{error}</div>
+                            ) : resultNavy ?
+                                <div>
+                                    <div className={styles.item}>
+                                        <h4 className={styles.h4}>Процент жира в теле</h4>
+                                        <div className={styles.table}>
+                                            <div className={styles.result}>
+                                                <p className={styles.h5}>Метод US Navy [более точный]</p>
+                                                <div className={styles.unit}>{resultNavy} %</div>
+                                            </div>
+                                            <div className={styles.result}>
+                                                <p className={styles.h5}>Метод BMI [менее точный]</p>
+                                                <div className={styles.unit}>{resultBMI} %</div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div className={styles.item}>
+                                        <h4 className={styles.h4}>Масса жира в теле</h4>
+                                        <div className={styles.result}>
+                                            <div className={styles.unit}>{resultFatMass} kg</div>
+                                        </div>
+                                    </div>
+
+                                    <div className={styles.item}>
+                                        <h4 className={styles.h4}>Ваша категория</h4>
+                                        <div className={styles.result}>
+                                            <div className={styles.unit}>{fatCategory}</div>
+                                        </div>
+                                    </div>
+
+                                </div> : <div className={styles.error}>Введите данные и нажмите рассчитать</div>}
+
                         </div>
                     </div>
                 </div>
