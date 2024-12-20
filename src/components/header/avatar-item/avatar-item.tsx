10b1cd84a9ff9738db130e63e068e1a46dfc85b8
Nikolay Fayustov <n.a.fayustov@gmail.com>
2024-12-20 21:30:42 +0300

    avat

--- src/components/header/avatar-item/avatar-item.tsx
+++ src/components/header/avatar-item/avatar-item.tsx
@@ -7,16 +7,18 @@ import Avatar from '../../../images/avatar_def.png'
 
 
 const AvatarItem = () => {
-    const avatar = useSelector((state: RootState) => state.userData.image);
+    const avatar = useSelector((state: RootState) => state.userData);
     const navigate = useNavigate();
 
     const handleClick = () => {
         navigate('/settings');
     };
 
+    const imageUrl = avatar.image ? avatar.image.replace('http://localhost', '') : Avatar;
+
     return (
         <div className={style.content} onClick={handleClick}>
-            <img src={avatar || Avatar} alt="User Avatar" />
+            <img src={imageUrl || Avatar} alt="User Avatar" />
         </div>
 
     );
